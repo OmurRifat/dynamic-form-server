276e3f619ae83e6a172cc47a51bac96a76485275
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-12-28 15:29:45 +0600

    some changes added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// Replace the uri string with your MongoDB deployment's connection string.
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qogqlqn.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
